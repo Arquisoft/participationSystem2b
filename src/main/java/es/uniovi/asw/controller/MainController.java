2f304131e8ced8070518a644568219bc46a8d4e8
Daniel Orviz <danielorviz@gmail.com>
2017-04-03 17:19:22 +0200

    añadido html

--- src/main/java/es/uniovi/asw/controller/MainController.java
+++ src/main/java/es/uniovi/asw/controller/MainController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,11 +46,12 @@ public class MainController {
         if(user!=null){
         	sesion.setAttribute("user", user);
         	List<Participant> lista = new ArrayList<Participant>();
-        	lista.add(new Participant("1", "", "", "", "", "", new Date(), ""));
-        	lista.add(new Participant("2", "", "", "", "", "", new Date(), ""));
-        	System.out.println("exito");
-        	model.addAttribute("lista",lista);
-        	return "principalUsuario";
+        	
+        	List<Suggestion> suggestions= new ArrayList<Suggestion>();// Service.getSuggestionService().getAllSuggestions();
+        	suggestions.add(new Suggestion((Participant)sesion.getAttribute("user"), "prueba", "jeje", "jaja"));
+        	suggestions.add(new Suggestion((Participant)sesion.getAttribute("user"), "Propuesta 2", "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas , las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.", "cateogria1"));
+        	model.addAttribute("suggestions",suggestions);
+        	return "suggestions";
         }
     	return "login";
     }
@@ -63,10 +65,15 @@ public class MainController {
     
     @RequestMapping("/mostrarPropuestas")
     public String mostrarPropuestas(HttpSession sesion,Model model, @ModelAttribute Message message) {
-    	System.out.println(sesion.getAttribute("user"));
-    	List<Suggestion> suggestions= new ArrayList<Suggestion>();// Service.getSuggestionService().getAllSuggestions();
-    	suggestions.add(new Suggestion((Participant)sesion.getAttribute("user"), "prueba", "jeje", "jaja"));
-    	model.addAttribute("suggestions",suggestions);
+    	// es de prueba, habria que llamr al servicefindAllSuggestions
+    	model.addAttribute("suggestions",((Participant)sesion.getAttribute("user")).getSuggestions());
+    	return "suggestions";
+    }
+    
+    @RequestMapping("/verPropuesta/{id}")
+    public String verPropuesta(HttpSession sesion,Model model, @PathVariable("id") String id, @ModelAttribute Message message) {
+    	// es de prueba, habria que llamr al servicefindAllSuggestions
+    	System.out.println(id);
     	return "suggestions";
     }
 
