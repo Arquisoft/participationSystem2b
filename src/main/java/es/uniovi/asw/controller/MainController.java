8a796b5d324301b3ff6a7b9c55294cf313930fc4
hugoPerezMartinez <hugovegadeo@gmail.com>
2017-04-04 17:41:11 +0200

    cada usuario va a una pagina distinta

--- src/main/java/es/uniovi/asw/controller/MainController.java
+++ src/main/java/es/uniovi/asw/controller/MainController.java
@@ -1,8 +1,5 @@
 package es.uniovi.asw.controller;
 
-
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -25,66 +22,73 @@ import es.uniovi.asw.service.Service;
 @Controller
 public class MainController {
 
-    @Autowired
-    private KafkaProducer kafkaProducer;
+	@Autowired
+	private KafkaProducer kafkaProducer;
 
-    @RequestMapping("/")
-    public String landing(Model model) {
-        model.addAttribute("message", new Message());
-        return "login";
-    }
-    
-    @RequestMapping("/send")
-    public String send(Model model, @ModelAttribute Message message) {
-        kafkaProducer.send("exampleTopic", message.getMessage());
-        return "redirect:/";
-    }
-    @RequestMapping(value = "/loguearse", 
-			method = RequestMethod.POST)
-    public String loguearse( HttpSession sesion,Model model,@RequestParam String usuario, @RequestParam String password) {
-        Participant user = Service.getParticipantService().findLogableUser(usuario,password);
-        if(user!=null){
-        	sesion.setAttribute("user", user);
+	@RequestMapping("/")
+	public String landing(Model model) {
+		model.addAttribute("message", new Message());
+		return "login";
+	}
 
-        	return "suggestions";
-        }
-    	return "login";
-    }
-    
-    @RequestMapping(value = "/registrar", 
-			method = RequestMethod.POST)
-    public String registrar(HttpSession sesion,Model model,@RequestParam String archivo) {
-        System.out.println(archivo);
-    	return "principalUsuario";
-    }
-    
-    @RequestMapping("/mostrarPropuestas")
-    public String mostrarPropuestas(HttpSession sesion,Model model) {
-    	// es de prueba, habria que llamr al servicefindAllSuggestions
-    //	model.addAttribute("suggestions",((Participant)sesion.getAttribute("user")).getSuggestions());
-    	return "suggestions";
-    }
-    @RequestMapping("/crearPropuesta")
-    public String crearPropuesta(HttpSession sesion,Model model) {
-    	return "addSuggestion";
-    }
-    @RequestMapping("/anadirPropuesta")
-    public String anadirPropuesta(HttpSession sesion,Model model,
-    		@RequestParam String titulo, @RequestParam String categoria,@RequestParam String propuesta ) {
-    	Suggestion sug = new Suggestion((Participant)sesion.getAttribute("user"), titulo, propuesta, categoria);
-    	Service.getSuggestionService().addSuggestion(sug);
-    	return "suggestions";
-    }
-    
-    @RequestMapping("/verPropuesta/{id}")
-    public String verPropuesta(HttpSession sesion,Model model, @PathVariable("id") Long id, @ModelAttribute Message message) {
-    	// es de prueba, habria que llamr al servicefindAllSuggestions
-    	model.addAttribute("suggestion",Service.getSuggestionService().findSugById(id));
-    	return "showSuggestion";
-    }
+	@RequestMapping("/send")
+	public String send(Model model, @ModelAttribute Message message) {
+		kafkaProducer.send("exampleTopic", message.getMessage());
+		return "redirect:/";
+	}
 
-    @ModelAttribute("suggestions")
-    public List<Suggestion> getSuggestions(){
-    	return Service.getSuggestionService().getAllSuggestions();
-    }
+	@RequestMapping(value = "/loguearse", method = RequestMethod.POST)
+	public String loguearse(HttpSession sesion, Model model, @RequestParam String usuario,
+			@RequestParam String password) {
+		Participant user = Service.getParticipantService().findLogableUser(usuario, password);
+		String resultado = "login";
+		if (user != null) {
+			if (user.getUsuario().equals("Admin1"))
+				resultado = "principalAdmin";
+			else
+				resultado = "principalUsuario";
+			
+			sesion.setAttribute(resultado, user);
+		}
+		return resultado;
+	}
+
+	@RequestMapping(value = "/registrar", method = RequestMethod.POST)
+	public String registrar(HttpSession sesion, Model model, @RequestParam String archivo) {
+		System.out.println(archivo);
+		return "principalUsuario";
+	}
+
+	@RequestMapping("/mostrarPropuestas")
+	public String mostrarPropuestas(HttpSession sesion, Model model) {
+		// es de prueba, habria que llamr al servicefindAllSuggestions
+		// model.addAttribute("suggestions",((Participant)sesion.getAttribute("user")).getSuggestions());
+		return "suggestions";
+	}
+
+	@RequestMapping("/crearPropuesta")
+	public String crearPropuesta(HttpSession sesion, Model model) {
+		return "addSuggestion";
+	}
+
+	@RequestMapping("/anadirPropuesta")
+	public String anadirPropuesta(HttpSession sesion, Model model, @RequestParam String titulo,
+			@RequestParam String categoria, @RequestParam String propuesta) {
+		Suggestion sug = new Suggestion((Participant) sesion.getAttribute("user"), titulo, propuesta, categoria);
+		Service.getSuggestionService().addSuggestion(sug);
+		return "suggestions";
+	}
+
+	@RequestMapping("/verPropuesta/{id}")
+	public String verPropuesta(HttpSession sesion, Model model, @PathVariable("id") Long id,
+			@ModelAttribute Message message) {
+		// es de prueba, habria que llamr al servicefindAllSuggestions
+		model.addAttribute("suggestion", Service.getSuggestionService().findSugById(id));
+		return "showSuggestion";
+	}
+
+	@ModelAttribute("suggestions")
+	public List<Suggestion> getSuggestions() {
+		return Service.getSuggestionService().getAllSuggestions();
+	}
 }
